818f0622568e3f0ba6d4436ac624da24d43c7946
pdemiray25 <pinardemiray25@gmail.com>
2025-10-21 19:17:03 +0200

    updated

---
+++ JsDeneme2.js
@@ -0,0 +1,19 @@
+/*let a = 5;
+a = a+6;
+console.log(a);
+let b = 9;
+b ++;
+console.log(b);*/
+
+let a = 7;
+a = 9*a;
+console.log(a);
+let x = 4 * 10 + 8;
+let y = ++x;
+
+console.log(y);
+let i=5*8+2;
+let z = i++;
+console.log(z);
+let greet="helo, I\'am here.";
+console.log(greet);
